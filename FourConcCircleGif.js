e342ba6af2e40062a2f07994b0bc0516d3f93c62
Anwesh43 <anweshthecool0@gmail.com>
2019-01-20 18:50:55 +0530

    adding State class

--- FourConcCircleGif.js
+++ FourConcCircleGif.js
@@ -61,3 +61,28 @@ const drawFCCNode = (context, i, scale) => {
     }
     restore()
 }
+
+class State {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += updateValue(this.scale, this.dir, lines, 1)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb(this.prevScale)
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
