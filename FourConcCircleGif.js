a589a30c821a7181eb7aa3b013f3c833b449ad13
Anwesh43 <anweshthecool0@gmail.com>
2019-01-20 18:31:35 +0530

    adding constants

---
+++ FourConcCircleGif.js
@@ -0,0 +1,9 @@
+const gifencoder = require('gifencoder')
+const Canvas = require('canvas').Canvas
+const w = 600, h = 600
+const nodes = 5
+const circles = 4
+const scGap = 0.05
+const scDiv = 0.51
+const sizeFactor = 3
+const strokeFactor = 90
